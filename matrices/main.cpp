8c70368634e841ed94155955906f8d335455c217
VashchilkoAV <vashchilko.av@phystech.edu>
2021-05-27 17:34:14 +0300

    [matrix]: benchmarks added, todo: fix bug with numTusks/numThreads < 2

--- matrices/main.cpp
+++ matrices/main.cpp
@@ -6,19 +6,56 @@
 #include <sstream>
 #include "matrix.h"
 
-const unsigned N = 4;
+class Timer {
+public:
+    Timer(unsigned blockSize, unsigned numThreads, std::string fname) : _blockSize(blockSize), _numThreads(numThreads){
+        std::ofstream _stream(fname);
+        begin = std::chrono::steady_clock::now();
+    }
+
+    ~Timer() {
+        auto end = std::chrono::steady_clock::now();
+        auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+        _stream << _numThreads << " " << _blockSize << " " << elapsed_ms.count() << " ms" << "\n";
+        std::cout << _numThreads << " " << _blockSize << " " << elapsed_ms.count() << " ms" << "\n";
+        _stream.flush();
+        _stream.close();
+    }
+
+private:
+    unsigned _blockSize, _numThreads;
+    std::ofstream _stream;
+    std::chrono::time_point<std::chrono::_V2::steady_clock, std::chrono::duration<long int, std::ratio<1, 1000000000>>>
+            begin;
+
+
+};
+
+void Test(Matrix &a, Matrix& b) {
+    std::ofstream result("result");
+    std::vector<unsigned> blockShapes = {2, 4, 8, 16, 32, 64, 128};
+    for (unsigned numThreads = 1; numThreads < 9; numThreads++) {
+        for (const auto& size : blockShapes) {
+            Timer timer(size, numThreads, "data.txt");
+            Matrix c = Multiply(a, b, numThreads, size, size, size, size);
+            result << c;
+        }
+    }
+}
+
+const unsigned N = 2048;
 
 int main() {
-    Matrix a(16, 16), b(16, 16);
-    a.ReadFromFile("m1.txt");
-    b.ReadFromFile("m1.txt");
-    //Matrix c = Multiply(a, b, 8, N, N, N, N);
-    //std::cout << c << std::endl;
+    Matrix a(N, N), b(N, N);
+    a.ReadFromFile("first");
+    b.ReadFromFile("second");
+    Test(a, b);
 
-    auto ranges = MakeRanges(8, 5);
-    for (const auto& it : ranges) {
-        std::cout << it.first << " " << it.second << std::endl;
-    }
+
+    //auto ranges = MakeRanges(8, 5);
+    //for (const auto& it : ranges) {
+    //    std::cout << it.first << " " << it.second << std::endl;
+    //}
 
     return 0;
 }
