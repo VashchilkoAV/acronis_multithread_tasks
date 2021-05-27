e319e49b02c5981915523f7633354e03f7197573
VashchilkoAV <vashchilko.av@phystech.edu>
2021-05-27 15:47:49 +0300

    [matrix]: added fix with numThreads > numTasks, todo: add benchmarks, fix bug with numTusks/numThreads < 2

--- matrices/main.cpp
+++ matrices/main.cpp
@@ -6,19 +6,19 @@
 #include <sstream>
 #include "matrix.h"
 
-const unsigned N = 8;
+const unsigned N = 4;
 
 int main() {
     Matrix a(16, 16), b(16, 16);
     a.ReadFromFile("m1.txt");
     b.ReadFromFile("m1.txt");
-    Matrix c = Multiply(a, b, 2, N, N, N, N);
-    std::cout << c << std::endl;
+    //Matrix c = Multiply(a, b, 8, N, N, N, N);
+    //std::cout << c << std::endl;
 
-    //auto ranges = MakeRanges(7, 2);
-    //for (const auto& it : ranges) {
-    //    std::cout << it.first << " " << it.second << std::endl;
-    //}
+    auto ranges = MakeRanges(8, 5);
+    for (const auto& it : ranges) {
+        std::cout << it.first << " " << it.second << std::endl;
+    }
 
     return 0;
 }
